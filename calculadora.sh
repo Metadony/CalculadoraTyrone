07a9b2cba299d244d94abf7fa45a75dad4907206
Metadony <tyrone.yusuki@gmail.com>
2024-07-15 12:33:55 +0100

    Create calculadora.sh
    Create file Calculadora script

---
+++ calculadora.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Diretorio atual
+
+
+echo 'Inico do script'
+
+echo "O diretorio atual é $PWD"
+
+
+#Execucao do script da calculadora
+
+
+python3 /home/metadony/calculadoratyrone/calculadora.py
